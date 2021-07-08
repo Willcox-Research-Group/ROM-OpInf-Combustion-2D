8b011795718484fb2abc721d5804a5ec76987752
Shane <shanemcq18@gmail.com>
2021-07-08 14:03:02 -0700

    lint data extraction codes

--- Extract_data_from_tar_files/extract_data_from_dat_to_float32.py
+++ Extract_data_from_tar_files/extract_data_from_dat_to_float32.py
@@ -1,48 +1,34 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import itertools
+import numpy as np
 
-gems_data = np.zeros((308184,10000),dtype = 'float32')
 
-def get_data(timestep,gems_data_col_no):
+gems_data = np.zeros((308184,10000), dtype='float32')
 
-    file = open('C:\\Users\\Admin\\Desktop\\storage\\combustion\\200000 to 209999\\test_file_ncons_'+str(timestep)+'.dat')
-    
-    lst = []
-    for line in file:
-        lst.append(line.strip())
-        
-    
-    lst1 = [i.split() for i in lst]
-    
-    def itertools_chain(a):
-        return list(itertools.chain.from_iterable(a))
-    
-    Line_data = itertools_chain(lst1)
-    
-    
+
+def get_data(timestep, gems_data_col_no):
+
+    with open('C:\\Users\\Admin\\Desktop\\storage\\combustion\\200000 to 209999\\test_file_ncons_'+str(timestep)+'.dat') as infile:
+        lst = [line.strip.split() for line in infile]
+
+    Line_data = list(itertools.chain.from_iterable(lst))
     kkk = np.array([float(i) for i in Line_data[32::]])
-    
+
     start = 0
     check = 308184
-    
+
     gems_data[:,gems_data_col_no] = kkk[start:check]
-    
     return gems_data
 
 
-for i in np.arange(200000, 209999,1):
+for i in np.arange(200000, 209999, 1):
     print(i)
-    data2 = get_data(i,i-200000)
-    
-    
-    
+    data2 = get_data(i, i - 200000)
+
 np.save('gems_extra_data20.npy', data2)
 
 data2 = np.load('gems_extra_data20.npy')
 data1 = np.load('gems50k.npy')
 
-a2 = np.concatenate((data1,data2), axis = 1)
+a2 = np.concatenate((data1, data2), axis=1)
 
 np.save('gems60k.npy',a2)
--- Extract_data_from_tar_files/extract_tar_files.py
+++ Extract_data_from_tar_files/extract_tar_files.py
@@ -1,26 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 28 22:28:51 2021
-
-@author: Admin
-"""
-
-
+# extract_tar_files.py
 import os
-import re
-import glob
-import h5py
-import shutil
-import logging
 import tarfile
-import numpy as np
-import multiprocessing as mp
-# change the current directory 
-# to specified directory 
+
+basefolder = r'C:\\Users\\Admin\\Desktop\\storage\\combustion\\'
+
+# change the current directory to specified directory
 folder_name_timesnapshots = '200000 to 209999'
-os.chdir(r'C:\\Users\\Admin\\Desktop\\storage\\combustion\\'+folder_name_timesnapshots) 
+os.chdir(os.path.join(basefolder, folder_name_timesnapshots))
 
 data_to_extract = 'Data_200000to209999.tar'
-import tarfile
-tar = tarfile.open("C:\\Users\\Admin\\Desktop\\storage\\combustion\\"+data_to_extract)
+tar = tarfile.open(os.path.join(basefolder, data_to_extract))
 tar.extractall()
+tar.close()
